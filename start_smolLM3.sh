07b097fa946163f24792cd6240e27e1b097f6d66
mohamedkhayat <132056305+mohamedkhayat@users.noreply.github.com>
2026-01-27 11:37:05 +0100

    added param to use jinja template

--- start_smolLM3.sh
+++ start_smolLM3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 HOST="${MODEL_HOST:-127.0.0.1}" 
-~/Apps/llama.cpp/build-b7577/bin/llama-server -m ~/models/smolLM3/SmolLM3-3B-128K-BF16.gguf -ngl 99 -fa on --port 8000 --host "$HOST" --ctx-size 128000
+~/Apps/llama.cpp/build-b7577/bin/llama-server -m ~/models/smolLM3/SmolLM3-3B-128K-BF16.gguf --jinja -ngl 99 -fa on --port 8000 --host "$HOST" --ctx-size 128000
